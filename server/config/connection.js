b716a802ca990d2daacbfa059cbaaabe5e24b955
deannapi <deannapi.mart@gmail.com>
2020-08-23 11:37:22 -0500

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const env = require('dotenv');
+dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
   useNewUrlParser: true,
