81dcabf74da14141c7a165311e7e9ec57ef4e365
deannapi <deannapi.mart@gmail.com>
2020-08-23 15:56:45 -0500

    deploy

---
+++ client/build/precache-manifest.942ed5099c0088dde6d9f7df3635665a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19db51cdbc61c790a828d65745b85ed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31f6fd925c92ff1f150d",
+    "url": "/static/css/main.49b21ed8.chunk.css"
+  },
+  {
+    "revision": "530578bb056afc2693d6",
+    "url": "/static/js/2.981114ce.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.981114ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f6fd925c92ff1f150d",
+    "url": "/static/js/main.a932482b.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
