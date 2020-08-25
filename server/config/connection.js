618c8c919a7a37fcaa91d47f335114e148fabf45
deannapi <deannapi.mart@gmail.com>
2020-08-25 13:32:58 -0500

    mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,11 +4,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', 
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false,
-  auth: {
-    user: 'deannapi',
-    password: 'M@th3141'
-  }
+  useFindAndModify: false
 });
 
 module.exports = mongoose.connection;
