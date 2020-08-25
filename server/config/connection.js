741854154ed37c9bfa0752a36621b77f44e62b7a
deannapi <deannapi.mart@gmail.com>
2020-08-25 13:17:52 -0500

    fixing mongo connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,11 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', 
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  auth: {
+    user: 'deannapi',
+    password: 'M@th3141'
+  }
 });
 
 module.exports = mongoose.connection;
