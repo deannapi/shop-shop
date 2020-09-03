9bd5e02dd689a011ea46d0fc046fec33c5a5d216
deannapi <deannapi.mart@gmail.com>
2020-09-03 11:44:03 -0500

    mongo url

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+const db = 'deannapi'
+const pswd = encodeURIComponent('Mart3141')
+
+const url = `mongodb://${db}:${pswd}@ds155596.mlab.com:55596/heroku_c9d38kpn`
+
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
