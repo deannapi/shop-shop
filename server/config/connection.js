e722d7faa321f8243b853f304bc4f0e96b5a4e02
deannapi <deannapi.mart@gmail.com>
2020-09-03 11:12:27 -0500

    adding mongoose error handling

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,6 +5,10 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', 
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
+}).then(() => {
+  console.log('mongoose success');
+}).catch((e) => {
+  console.log('mongoose error: ', e.message)
 });
 
 module.exports = mongoose.connection;
